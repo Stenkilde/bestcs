5c7bcb2235505d6293467118805823e9333c6dbc
Stenkilde <hello@stenkil.de>
2017-07-06 15:39:13 +0200

    Added enviroment aware URLs

--- src/services/videos.js
+++ src/services/videos.js
@@ -1,12 +1,20 @@
 var request = require('./request.js');
 
+let __API__ = '';
+
+if ( process.env.NODE_ENV === 'production') {
+    __API__ = 'https://api.loungedinosaur.dk';
+} else {
+    __API__ = 'http://localhost:8000';
+}
+
 /**
  * Get collection of articles
  * @param options
  * @returns {axios.Promise|Promise.<T>|*}
  */
 function collection() {
-    return request.get('http://api.loungedinosaur.dk/videos')
+    return request.get(__API__ + '/videos')
         .then(function(response) {
             return {
                 collection: response
@@ -15,7 +23,8 @@ function collection() {
 }
 
 function get(title) {
-    return request.get('http://api.loungedinosaur.dk/video?title=' + title)
+    console.log(process.env);
+    return request.get(__API__ + '/video?title=' + title)
         .then(function(response) {
             return {
                 video: response
@@ -24,7 +33,7 @@ function get(title) {
 }
 
 function typeahead(search) {
-    return request.get('http://api.loungedinosaur.dk/typeahead?search=' + search)
+    return request.get(__API__ + '/typeahead?search=' + search)
         .then(function(response) {
             return {
                 search: response
