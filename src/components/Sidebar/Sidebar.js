f0642b73db7ec49374e93e8863479b00126173db
Stenkilde <hello@stenkil.de>
2017-07-10 09:57:52 +0200

    Progress with sidebar

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+
+class SideBar extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="sidebar">
+                <h3>Recommended</h3>
+                
+            </div>
+        );
+    }
+}
+
+export default SideBar;
